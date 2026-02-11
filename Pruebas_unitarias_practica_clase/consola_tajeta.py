aa5c6fe712856e940c6a694ef55db8fa573a42b0
Sebastian <srendon521@gmail.com>
2026-02-11 09:23:57 -0500

    Agrego carpeta Pruebas_unitarias_practica_clase y otros archivos

--- Pruebas_unitarias_practica_clase/consola_tajeta.py
+++ Pruebas_unitarias_practica_clase/consola_tajeta.py
@@ -1,8 +1,9 @@
+import logica_tarjeta
 
 compra= input("ingrese el valor de la compra: ")
 interes = input("ingrese el valor del interés mensual en porcentaje: ")
 plazo = input("ingrese el número de meses para pagar la compra: ")
 
-cuota = calcular_cuota(compra, interes, plazo)
+cuota = logica_tarjeta.calcular_cuota(compra, interes, plazo)
 
 print( cuota)
