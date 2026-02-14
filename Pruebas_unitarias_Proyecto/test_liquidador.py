28b924dd906d82be2c70a432ac67d8b35754ba6d
Sebastian Rendon <srendon521@gmail.com>
2026-02-13 19:02:49 -0500

    Se agrego la estructura de pruebas con unittest

---
+++ Pruebas_unitarias_Proyecto/test_liquidador.py
@@ -0,0 +1,29 @@
+import unittest
+import logica_liquidador
+
+class TestCalculoSalario(unittest.TestCase):
+
+    def test_normal1_salario(self):
+
+        #entradas
+        salario = 5000000
+        horas_extra = 150000
+        bonificaciones = 50000
+        comisiones = 100000
+        auxilios = 0
+        salud = 0.04
+        pension = 0.04
+        impuesto_dinero = 50000
+
+        #proceso
+        salario_calculado = logica_liquidador.calcular_salario(salario, horas_extra, bonificaciones, comisiones, auxilios, salud, pension, impuesto_dinero)
+
+        #Datos de salida esperados
+        salario_esperado = 4850000
+
+        #verificar la salida
+        self.assertEqual(salario_calculado, salario_esperado)
+
+
+if __name__ == '__main__':
+    unittest.main()
