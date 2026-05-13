9e0f0ecbc316badb6799196b344757296f7e2c15
Sebastian Rendon <srendon521@gmail.com>
2026-05-13 14:26:11 -0500

    Casos de prueba para insertar creados

--- tests/test_liquidador_db.py
+++ tests/test_liquidador_db.py
@@ -9,16 +9,16 @@ class TestLiquidacion(unittest.TestCase):
     def test_insert_1(self):
         # Crear una liquidacion
         liquidacion = Liquidacion(
-            salario=2000000,
-            horas_extra=100000,
-            bonificaciones=50000,
-            comisiones=30000,
-            auxilios=100000,
+            salario= 5000000,
+            horas_extra= 150000,
+            bonificaciones= 50000,
+            comisiones= 100000,
+            auxilios= 0,
             porcentaje_salud=4,
             porcentaje_pension=4,
             impuestos=50000,
-            total_devengado=2280000,
-            salario_neto=2070000
+            total_devengado=5300000,
+            salario_neto=4850000
         )
 
         # Guardarla en la BD
@@ -28,6 +28,58 @@ class TestLiquidacion(unittest.TestCase):
         liquidacion_buscada = LiquidacionesController.buscar_liquidacion(liquidacion.id)
 
         # Verificar si la trajo bien
+        self.assertTrue(liquidacion.is_equal(liquidacion_buscada))
+
+
+    def test_insert_2(self):
+        liquidacion = Liquidacion(
+            salario= 6200000,
+            horas_extra= 100000,
+            bonificaciones= 100000,
+            comisiones= 20000,
+            auxilios= 0,
+            porcentaje_salud=4,
+            porcentaje_pension=4, 
+            impuestos=62000,
+            total_devengado=6420000, 
+            salario_neto=5862000
+        )
+        LiquidacionesController.insertar(liquidacion)
+        liquidacion_buscada = LiquidacionesController.buscar_liquidacion(liquidacion.id)
+        self.assertTrue(liquidacion.is_equal(liquidacion_buscada))
+
+    def test_insert_3(self):
+        liquidacion = Liquidacion(
+            salario= 4500000,
+            horas_extra= 50000,
+            bonificaciones= 50000,
+            comisiones= 20000,
+            auxilios= 0,
+            porcentaje_salud=4,
+            porcentaje_pension=4, 
+            impuestos=45000,
+            total_devengado=4620000, 
+            salario_neto=4215000
+        )
+        LiquidacionesController.insertar(liquidacion)
+        liquidacion_buscada = LiquidacionesController.buscar_liquidacion(liquidacion.id)
+        self.assertTrue(liquidacion.is_equal(liquidacion_buscada))
+
+    def test_insert_4(self):
+        liquidacion = Liquidacion(
+            salario= 4000000,
+            horas_extra= 20000,
+            bonificaciones= 60000,
+            comisiones= 10000,
+            auxilios= 0,
+            porcentaje_salud=4,
+            porcentaje_pension=4, 
+            impuestos=40000,
+            total_devengado=4270000, 
+            salario_neto=3910000
+        )
+        LiquidacionesController.insertar(liquidacion)
+        liquidacion_buscada = LiquidacionesController.buscar_liquidacion(liquidacion.id)
         self.assertTrue(liquidacion.is_equal(liquidacion_buscada))
 
 if __name__ == '__main__':
