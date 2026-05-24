e76fc3a8b61845c318eac52177282106b38c3ebb
Sebastian Rendon <srendon521@gmail.com>
2026-05-24 18:30:34 -0500

    Funcionalidad de crear tablas desde la web

--- app.py
+++ app.py
@@ -3,11 +3,18 @@ from flask import render_template, request
 import sys
 sys.path.append("src")
 
+from controller.liquidaciones_controller import LiquidacionesController
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/crear_tablas')
+def crear_tablas():
+    LiquidacionesController.crear_tabla()
+    return "Tablas creadas exitosamente"
 
 if __name__ == '__main__':
     app.run(debug=True)
