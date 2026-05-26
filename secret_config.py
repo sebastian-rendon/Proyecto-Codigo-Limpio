8ad86672f53f98854a4bed215fb27ef090e36ab9
Sebastian Rendon <srendon521@gmail.com>
2026-05-26 17:58:17 -0500

    fix: eliminar secret_config.py del repositorio

--- secret_config.py
+++
@@ -1,7 +0,0 @@
-# Recuerde poner aqui los datos de conexion y guardar como SecretConfig.py
-
-PGHOST='dpg-d7v2tu9kh4rs739nbe8g-a.virginia-postgres.render.com'
-PGDATABASE='liquidador_nomina_67mk'
-PGUSER='liquidador_nomina_67mk_user'
-PGPASSWORD='lVWtEYZko9Bxa2b5Tc3igH9aQwb0Fs5P'
-PGPORT=5432
