57140e2084170251d453fe6218b73a94c04d7fa0
Sebastian Rendon <srendon521@gmail.com>
2026-05-29 19:43:51 -0500

    .

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ sys.path.append("src")
 
 from view.web import vista_liquidaciones
 
+# Flask constructor: crea una variable que servira para comunicar el servidor con el cliente, es decir, con el navegador
 app = Flask(__name__)
 
 app.register_blueprint(vista_liquidaciones.blueprint)
 
+#permite que la aplicacion se ejecute indipendientemente, es decir, que se ejecute solo si se ejecuta este archivo y no si se importa desde otro archivo
 if __name__ == '__main__':
     app.run(debug=True)
